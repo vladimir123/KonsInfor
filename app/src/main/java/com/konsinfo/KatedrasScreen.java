0f58e103e2de47e078003c3e838b2fa53ee4998b
vladimir123 <novi4ok123@hotmail.com>
2017-11-27 23:49:02 +0200

    added katedras sarakstu

--- app/src/main/java/com/konsinfo/KatedrasScreen.java
+++ app/src/main/java/com/konsinfo/KatedrasScreen.java
@@ -1,6 +1,5 @@
 package com.konsinfo;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -12,17 +11,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 /**
  * Created by vladimir on 27.11.2017.
  */
 
-public class MenuScreen extends AppCompatActivity {
+public class KatedrasScreen extends AppCompatActivity {
 
-    Button btn_kons;
-    Button btn_lekcii;
     private TextView mTextMessage;
+    Button btn_iti, btn_vik, btn_mik, btn_others;
 
     //menu items
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -32,53 +28,41 @@ public class MenuScreen extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    Intent intent = new Intent(MenuScreen.this, MenuScreen.class);
+                    Intent intent = new Intent(KatedrasScreen.this, MenuScreen.class);
                     startActivity(intent);
-                    finish();
+//                    finish();
                     return true;
                 case R.id.navigation_about:
                     mTextMessage.setVisibility(View.VISIBLE);
                     mTextMessage.setText(R.string.txt_about);
-                    btn_kons.setVisibility(View.INVISIBLE);
-                    btn_lekcii.setVisibility(View.INVISIBLE);
+                    btn_iti.setVisibility(View.INVISIBLE);
+                    btn_mik.setVisibility(View.INVISIBLE);
+                    btn_vik.setVisibility(View.INVISIBLE);
+                    btn_others.setVisibility(View.INVISIBLE);
                     return true;
             }
             return false;
         }
     };
 
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu_screen);
+        setContentView(R.layout.katedras_list);
         //set listener on menu item click
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        mTextMessage = (TextView)findViewById(R.id.textView);
-        btn_kons = (Button)findViewById(R.id.btn_kons);
-        btn_lekcii = (Button)findViewById(R.id.btn_lekcii);
+        mTextMessage = (TextView)findViewById(R.id.textView2);
+        btn_iti = (Button)findViewById(R.id.btn_iti);
+        btn_mik = (Button)findViewById(R.id.btn_mik);
+        btn_vik = (Button)findViewById(R.id.btn_vik);
+        btn_others = (Button)findViewById(R.id.btn_others);
 
-        btn_kons.setText(R.string.btn_kons);
-        btn_lekcii.setText(R.string.btn_lekcii);
-
-        //show Konsultacijas list
-        btn_kons.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MenuScreen.this, KatedrasScreen.class);
-                startActivity(intent);
-//                finish();
-            }
-        });
-
-        //show lekcijas list
-        btn_lekcii.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ///
-            }
-        });
+        btn_iti.setText(R.string.btn_iti);
+        btn_mik.setText(R.string.btn_mik);
+        btn_vik.setText(R.string.btn_vik);
+        btn_others.setText(R.string.btn_others);
     }
+
 }
